149a9c97face57dd30c2d5f22b3f36638eaf22ea
Ranieri Queiroz <105569169+Allternatelab@users.noreply.github.com>
2023-09-25 23:18:27 -0300

    Update Instalar Mod Allternate.bat

--- Instalar Mod Allternate.bat
+++ Instalar Mod Allternate.bat
@@ -1,6 +1,6 @@
 @echo off
 
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = 3072}; """"& { $(Invoke-WebRequest -UseBasicParsing 'C:\ALLTERNATE\SPOTIFYMOD/Install.ps1')} -new_theme """" | Invoke-Expression"
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = 3072}; """"& { $(Invoke-WebRequest -UseBasicParsing 'https://github.com/Allternatelab/SpotiMod2/blob/PROJETOS/Install.ps1')} -new_theme """" | Invoke-Expression"
 
 pause
 exit /b
